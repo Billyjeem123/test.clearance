35d93e75d7c3bac30723418907c18ee1339bae8b
Billyjeem123 <billyhadiattaofeeq@gmail.com>
2024-06-20 11:42:30 +0100

    all updat to git

---
+++ app/Mail/DocumentStatusUpdated.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Mail;
+
+use App\Models\Document;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Mail\Mailables\Content;
+use Illuminate\Mail\Mailables\Envelope;
+use Illuminate\Queue\SerializesModels;
+
+class DocumentStatusUpdated extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    public $document;
+    public $status;
+
+
+    /**
+     * Create a new message instance.
+     */
+    public function __construct(Document $document, $status)
+    {
+        $this->document = $document;
+        $this->status = $status;
+    }
+
+    /**
+     * Get the message envelope.
+     */
+    public function build()
+    {
+        return $this->view('emails.document_status_updated')
+            ->with([
+                'documentId' => $this->document->id,
+                'status' => $this->status,
+            ]);
+    }
+
+    /**
+     * Get the message content definition.
+     */
+    public function content(): Content
+    {
+        return new Content(
+            view: 'view.name',
+        );
+    }
+
+    /**
+     * Get the attachments for the message.
+     *
+     * @return array<int, \Illuminate\Mail\Mailables\Attachment>
+     */
+    public function attachments(): array
+    {
+        return [];
+    }
+}
---
+++ app/Models/DocumentApproval.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class DocumentApproval extends Model
+{
+    use HasFactory;
+
+
+    public function approvals()
+    {
+        return $this->hasMany(DocumentApproval::class);
+    }
+
+    public function document()
+    {
+        return $this->belongsTo(Document::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+
+}
